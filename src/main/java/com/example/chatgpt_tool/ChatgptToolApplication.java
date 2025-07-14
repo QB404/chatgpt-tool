7808c13caccc2d19e61d5ffff5aa9b86a970dcdb
QB404 <130790005+QB404@users.noreply.github.com>
2025-07-14 21:08:24 +0800

    add secret config file

--- src/main/java/com/example/chatgpt_tool/ChatgptToolApplication.java
+++ src/main/java/com/example/chatgpt_tool/ChatgptToolApplication.java
@@ -2,8 +2,10 @@ package com.example.chatgpt_tool;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:secret.properties")
 public class ChatgptToolApplication {
 
 	public static void main(String[] args) {
